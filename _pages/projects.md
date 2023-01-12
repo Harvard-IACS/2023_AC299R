5e16173f2d9453276af84eb1074ada05ed6c79d2
Vishnu <m.vishnu701@gmail.com>
2023-01-12 21:41:35 +0530

    added navigation bar

--- _pages/projects.md
+++ _pages/projects.md
@@ -5,6 +5,7 @@ collection: projects
 permalink: /projects/
 entries_layout: grid
 classes: wide
+sort_by: sequence
 header: 
   overlay_image: /assets/images/bannertest1.jpeg
 ---
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Project 1",
+        "excerpt":"Learning from data in order to gain useful predictions and insights. This first iteration of the course introduces five key facets of an investigation: data wrangling, cleaning, and sampling to get a suitable data set; data management to be able to access big data quickly and reliably; exploratory data analysis...","categories": [],
+        "tags": [],
+        "url": "/projects/2013/",
+        "teaser": "/assets/images/header/HeaderDesign1.jpeg"
+      },{
+        "title": "Project 2",
+        "excerpt":"2014 CS109A: Harvard's Data Science Learning from data in order to gain useful predictions and insights. This second iteration of the course continues to use methods for five key facets of an investigation: data wrangling, cleaning, and sampling to get a suitable data set; data management to be able to...","categories": [],
+        "tags": [],
+        "url": "/projects/2014/",
+        "teaser": "/assets/images/header/HeaderDesign1.jpeg"
+      },{
+        "title": "Project 3",
+        "excerpt":"2015 CS109A: Harvard's Data Science Learning from data in order to gain useful predictions and insights. This third iteration of the course continues on the same ideas as the previous two; use methods of the five key facets of an investigation: data wrangling, cleaning, and sampling to get a suitable...","categories": [],
+        "tags": [],
+        "url": "/projects/2015/",
+        "teaser": "/assets/images/header/HeaderDesign1.jpeg"
+      }]
